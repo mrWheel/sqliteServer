af62e4058ccd45c64324e023e912e419136c1441
Willem Aandewiel <Willem@Aandewiel.nl>
2026-01-19 12:34:21 +0100

    Remove obolite files and move scripts

--- obsolite/db_psram/db_psram.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "esp_err.h"
-#include "sqlite3.h"
-
-esp_err_t db_psram_open(sqlite3** out_db);
--- obsolite/sql_api/sql_api.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "esp_err.h"
-#include "sqlite3.h"
-
-esp_err_t sql_api_start(sqlite3* db);
